503962820035cef93793980f76f09cc1e36909e6
rommelbarbieto1 <rommel.barbieto@torontomu.ca>
2024-10-09 20:14:36 -0400

    update

--- lab 5 ITM780.js
+++ lab 5 ITM780.js
@@ -1,11 +1,22 @@
-function showNamePrompt(){
-    const name = promtpt("Enter name:")
+document.getElementById("nameButton").onclick = function() {
+    let name = prompt("Enter name:");
     if (name) {
-        document.getElementById("displayName").innerText = name;
+        document.getElementById("nameDisplay").innerText = name
+    } else {
+        document.getElementById("nameDisplay").innerText = "Invalid. Please enter name."
+
     }
+};
+
+document.getElementaryById("Image").onclick = function() {
+    this.style.width = "200px";
+    this.style.height = "200px";
+};
+
+function hover(element) {
+    element.style.color = "04AA6D"
 }
 
-function enlargeImage(){
-    const image = document.getElementById("Image");
-    image.style.transform = "scale(1.5)";
+function unhover(element) {
+    element.style.color = "";
 }
