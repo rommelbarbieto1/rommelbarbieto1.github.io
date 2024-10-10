8583769fd0b1641c1a4144cdd9c46c0cd6e99f0e
rommelbarbieto1 <rommel.barbieto@torontomu.ca>
2024-10-09 21:54:10 -0400

    edit

--- lab 5 ITM780.js
+++ lab 5 ITM780.js
@@ -1,3 +1,4 @@
+// Create a button that will propmt a user to type in thie rname on my homepage.
 document.getElementById("nameButton").onclick = function() {
     let name = prompt("Enter name:");
     if (name) {
@@ -7,16 +8,16 @@ document.getElementById("nameButton").onclick = function() {
 
     }
 };
-
+// Create an image and make sure it is enlarged when clicked.
 document.getElementaryById("Image").onclick = function() {
     this.style.width = "50px";
     this.style.height = "50px";
 };
-
+//Create a navigation menu with at least three navigation items and two menu items. 
 function hover(element) {
     element.style.color = "04AA6D"
 }
-
+//Include an onmouseover/onmouseout event for your nav items.//
 function unhover(element) {
     element.style.color = "";
 }
